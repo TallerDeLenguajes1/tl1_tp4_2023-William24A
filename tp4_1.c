840bac3ccc7d3ccd405911b426b1ed9e8ac7c3d2
“Gabi” <mgechazu@gmail.com>
2023-04-13 19:19:49 -0300

    se realizo la estructura e ingresamos datos en el tp4_2

--- tp4_1.c
+++ tp4_1.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 struct Tarea {
-int TareaID; //Numerado en ciclo iterativo
-char *Descripcion;
-int Duracion; // entre 10 – 100
+    int TareaID; //Numerado en ciclo iterativo
+    char *Descripcion;
+    int Duracion; // entre 10 – 100
 };
 
-int main(){
-    int can;
-    printf("Ingresar cantidad de tareas: ");
-    scanf("%d", &can);
+typedef struct Tarea Tareas;
 
+int main(){
+    int can,dur;
+    char burr[100];
+    printf("Ingresar cantidad de tareas: ");
+    scanf("%d", &can); //Reserva de memoria de un vector de punteros
+    Tareas **tarea=(struct Tarea**)malloc(sizeof(struct Tarea*)*can);
+    
+    for(int i=0;i<can;i++){
+        tarea[i]= NULL;
+    }
+
+    for(int i=0;i<can;i++){
+        tarea[i]= malloc(sizeof(Tareas));//Muy importante hay que reservar memoria para que cada puntero apunte a una estructura 
+        tarea[i]->TareaID=i+1;              //en el caso de arriba se le indica a donde debe apuntar 
+        printf("Ingrese la duracion(10-100):");
+        scanf("%d", &dur);
+        fflush(stdin);
+        tarea[i]->Duracion=dur;
+
+        printf("Ingrese la descripcion de la tarea: ");
+        gets(burr);
+        fflush(stdin); //Tercera reserva de memoria
+        tarea[i]->Descripcion = (char *) malloc(sizeof(char)*strlen(burr)+1);
+        strcpy(tarea[i]->Descripcion, burr);
+    }
+
+    //poner al final
+    for(int i=0;i<can;i++){ //Hay que limpiar todas las reservas
+        free(tarea[i]->Descripcion);
+        free(tarea[i]);
+    }
+    free(tarea);
     return 0;
 }
